7e7a71c7d191deb28763509fa5fe72925127e425
chani529 <chani529@gmail.com>
2024-04-18 18:03:29 +0900

    wip

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -3,22 +3,21 @@ import { Outlet } from "react-router";
 import { LayoutDefaultProps } from "@type/index";
 import { menuList } from "@route/MenuList";
 import SidebarComponent from "./sidebar/SidebarComponent";
+import Header from "./Header";
 
-const DefaultLayout = ({ children }: LayoutDefaultProps) => {
-  return (
+const DefaultLayout = ({ children }: LayoutDefaultProps) => (
     <>
-      <div className="wrapper">
-        <SidebarComponent menuList={menuList} />
-        <div>
-          <div>TEST</div>
-          <div className="content-wrapper">
-            {/* children이 있을경우는 children을 없을 경우에는 Outlet을 나타내준다 */}
-            {children || <Outlet />}
-          </div>
+        <div className="wrapper">
+            <SidebarComponent menuList={menuList} />
+            <div className="main-wrapper">
+                <Header props={menuList} />
+                <div className="content-wrapper">
+                    {/* children이 있을경우는 children을 없을 경우에는 Outlet을 나타내준다 */}
+                    {children || <Outlet />}
+                </div>
+            </div>
         </div>
-      </div>
     </>
-  );
-};
+);
 
 export default DefaultLayout;
